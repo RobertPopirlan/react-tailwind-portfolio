bd98fd85bb5738cdcbf731bf032601e5e1a2c0a9
Robert <you@example.com>
2023-12-03 23:31:02 +0200

    project is done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,9 +8,9 @@ const About = () => {
 <p className='text-4xl font-bold inline border-b-4 border-gray-500' >About</p>
 </div> 
 <div >
-    <p className='text-xl mt-20'>The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound. [ding] Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start.</p>
+    <p className='text-xl mt-20 px-4'>The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound. [ding] Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start.</p>
 <br/>
-<p className='text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, repudiandae cum non hic repellendus at sapiente tempora esse enim id assumenda neque similique, in magni tenetur delectus. Molestias, laudantium perspiciatis.
+<p className='text-xl px-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, repudiandae cum non hic repellendus at sapiente tempora esse enim id assumenda neque similique, in magni tenetur delectus. Molestias, laudantium perspiciatis.
 </p>
 
 </div>
