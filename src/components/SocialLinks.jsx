e37db3160a907cda8023707bdf3980e1bf51cefa
Robert <you@example.com>
2023-12-03 23:19:27 +0200

    project is done

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -57,7 +57,7 @@ const SocialLinks = () => {
     <div className="hidden   lg:flex flex-col top-[35%] left-0 fixed"> 
 <ul>
     {links.map(({id,child,href,style,download})=>(
-         <li key={id} className={'flex justify-between items-center w-40 h-14 px-4 bg-transparent ml-[-100px] hover:ml-[-10px] hover:rounder-md duration-300'+" "+style}>
+         <li key={id} className={'flex justify-between items-center w-40 h-14 px-4 bg-transparent ml-[-100px] hover:ml-[-10px] hover:rounder-md duration-300'}>
          <a href={href}  className='flex justify-between items-center w-full text-white'download={download} target='_blank' >
          {child}
          </a>
